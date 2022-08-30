cf52f449ba6f348a0bb011d808e5261ff37b9ff9
maxi disilvestro <maxidisilvestro@gmail.com>
2022-08-30 17:54:39 -0300

    prueba coneccion a db

--- src/db.ts
+++ src/db.ts
@@ -11,10 +11,10 @@ require('dotenv').config()
 
 // })
 export const sequelize = new Sequelize({
-    database:'daiutfsqr987da',
-    username:'afxnmftfheqyzz',
-    password:'7eba79acb21e113199fd2ab3c5960ecb93a15bfadf475c61f4935dc871fdfd49',
-    host:'ec2-54-161-255-125.compute-1.amazonaws.com',
+    database:'dbbrqr2j3gb2ns',
+    username:'rymkijagqdxitw',
+    password:'8313b1932d1e5d1ec666e52b9776e03d0ece1769bd0d0ef1f564bd5958819780',
+    host:'ec2-3-224-184-9.compute-1.amazonaws.com',
     dialect:'postgres',
     port:5432
 
