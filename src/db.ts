0050d4a8877a56a8a713b39f83334b7651a0a424
maxi disilvestro <maxidisilvestro@gmail.com>
2022-08-30 21:18:48 -0300

    prueba 1.7

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,7 @@
 // import lowdb from 'lowdb'
 // import FileSync from 'lowdb/adapters/FileSync'
 
-import {Sequelize} from "sequelize";
+import {Sequelize} from "Sequelize";
 // var parse = require('pg-connection-string').parse
 // var config = parse('postgres://afxnmftfheqyzz:7eba79acb21e113199fd2ab3c5960ecb93a15bfadf475c61f4935dc871fdfd49@ec2-54-161-255-125.compute-1.amazonaws.com:5432/daiutfsqr987da')
 require('dotenv').config()
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import app from './app'
 import {sequelize} from './db'
-import '../src/component/projects/models'
+//import '../src/component/projects/models'
 //import { createRole } from './component/roles/controller'
 async function main(){
     try {
-    await sequelize.sync({force: false, alter: false})
+    //await sequelize.sync({force: false, alter: false})
     app.listen(app.get('port'))
     //createRole()
     console.log('server listening on port: 4000')
