0b10fbe15f4901e8ae74fc17bb455523eb15535b
maxi disilvestro <maxidisilvestro@gmail.com>
2022-09-02 14:58:36 -0300

    prueba con variables de entorno

--- src/db.ts
+++ src/db.ts
@@ -2,13 +2,15 @@ import {Sequelize} from "sequelize";
 require('dotenv').config()
 
 console.log("*******",process.env.DATABASE_URL)
-  export const sequelize= new Sequelize(process.env.DATABASE_URL||'postgres://rymkijagqdxitw:8313b1932d1e5d1ec666e52b9776e03d0ece1769bd0d0ef1f564bd5958819780@ec2-3-224-184-9.compute-1.amazonaws.com:5432/dbbrqr2j3gb2ns',{
-    dialectOptions:{
-        ssl:{
-                rejectUnauthorized:false
-        }
-    }
- })
+  export const sequelize= new Sequelize(process.env.DATABASE_URL||'postgres://rymkijagqdxitw:8313b1932d1e5d1ec666e52b9776e03d0ece1769bd0d0ef1f564bd5958819780@ec2-3-224-184-9.compute-1.amazonaws.com:5432/dbbrqr2j3gb2ns',
+  //{
+    // dialectOptions:{
+    //     ssl:{
+    //             rejectUnauthorized:false
+    //     }
+    // }
+// }
+ )
 //     console.log("estoy en el else")
 //     const sequelize= new Sequelize({
 //     database:'daiutfsqr987da',
