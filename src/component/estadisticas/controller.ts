81a857e93f9b05db18f8ea27b5d12febfaa1b6b5
maxi disilvestro <maxidisilvestro@gmail.com>
2023-11-28 02:51:42 -0300

    estadisticas contratos

--- src/component/estadisticas/controller.ts
+++ src/component/estadisticas/controller.ts
@@ -2,9 +2,10 @@ import { Request, Response } from 'express'
 import { Event } from '../events/models';
 import { Carga } from '../clientes/precarga/models'
 import { Contrato } from '../contrato/model'
-import { Op } from 'sequelize';
+import { Op} from 'sequelize';
 import { Vendors } from '../vendors/models';
-
+import { Pago } from '../cobranza/model';
+const Sequelize = require('sequelize');
 interface ObjetoFinal {
   anio: string;
   mes: string;
@@ -89,7 +90,6 @@ export async function getContratos(req: Request, res: Response) {
 export async function getEventos(req: Request, res: Response) {
   try {
     const gerente=req.headers["user-id"]
-    console.log("eventos",gerente)
     const a = await Vendors.findByPk(Number(gerente))
     if(Number(a?.getDataValue("category"))===3||Number(a?.getDataValue("category"))===4){
         const vens = await Vendors.findAll({where:{
@@ -256,3 +256,202 @@ async function entrada(data: any, tipo: number,gerente:any) {
     }
 }
 
+
+
+
+// export async function getContratosFiltrados(req: Request, res: Response) {
+//   try {
+//       const gerente = req.headers["user-id"];
+//       console.log(">>>>>>>>>>>>>><<<",gerente)
+//       const events = await Contrato.findAll({
+//         where: {
+//             vendedor: gerente?.toString()
+//         }
+//     })
+//     const events2 = await Pago.findAll({
+//       where: {
+//         vendedor: gerente?.toString()
+//     }
+//     })
+// const data=[]
+// const data2=[]
+// for (let i = 0; i < events2.length; i++) {
+//   const element ={
+//     pagos: events2[i].dataValues.dia
+//   } 
+//   data2.push(element)
+// }
+// for (let i = 0; i < events.length; i++) {
+//   const element = {
+//     dia:events[i].dataValues.dia,
+//     metodopago:events[i].dataValues.metodopago,
+//     adeudado:events[i].dataValues.adeudado,
+//   };
+//   data.push(element)
+// }
+// // console.log(organizarDatos(data))
+// // console.log(organizarDatos(data2))
+// // console.log(data2)
+// const resultadoTransformado = data
+// return res.status(200).json(organizarDatos(data,data2))
+
+//   } catch (error) {
+//       console.log("ERROR", error);
+//       return res.status(500).send("Error interno del servidor");
+//   }
+// }
+
+
+export async function getContratosFiltrados(req: Request, res: Response) {
+  try {
+    const vendedor = req.headers["user-id"];
+    const a = await Vendors.findByPk(Number(vendedor))
+    console.log(a?.dataValues.category)
+    if(a?.dataValues.category=="1"||a?.dataValues.category=="2"){
+       const contratos = await Contrato.findAll({
+      where: {
+        vendedor: vendedor?.toString()
+      }
+    });
+
+    const pagos = await Pago.findAll({
+      where: {
+        vendedor: vendedor?.toString()
+      }
+    });
+
+const contratosTransformados = contratos.map((evento) => ({
+      dia: evento.dataValues.dia,
+      metodopago: evento.dataValues.metodopago,
+      adeudado: evento.dataValues.adeudado,
+    }));
+
+    const pagosTransformados = pagos.map((evento) => ({
+      pagos: evento.dataValues.dia,
+    }));
+      const resultadoTransformado = organizarDatos(contratosTransformados, pagosTransformados);
+       return res.status(200).json(resultadoTransformado);
+    }
+    if(a?.dataValues.category=="5"){
+      const contratos = await Contrato.findAll();
+
+   const pagos = await Pago.findAll();
+
+const contratosTransformados = contratos.map((evento) => ({
+     dia: evento.dataValues.dia,
+     metodopago: evento.dataValues.metodopago,
+     adeudado: evento.dataValues.adeudado,
+   }));
+
+   const pagosTransformados = pagos.map((evento) => ({
+     pagos: evento.dataValues.dia,
+   }));
+     const resultadoTransformado = organizarDatos(contratosTransformados, pagosTransformados);
+      return res.status(200).json(resultadoTransformado);
+   }
+   if(a?.dataValues.category=="4"){
+    const b = await Vendors.findAll({
+        where:{
+        gerente:vendedor?.toString()
+        }
+      })
+    
+      const ven = b.map((evento) => (
+        evento.dataValues.id?.toString()
+      ));
+const contratos = await Contrato.findAll({
+  where: {
+    vendedor: {[Op.in]: ven}
+  }
+   });
+   
+ const pagos = await Pago.findAll(
+  {
+    where: {
+      vendedor: {[Op.in]: ven}
+    }
+     }
+ );
+
+const contratosTransformados = contratos.map((evento) => ({
+   dia: evento.dataValues.dia,
+   metodopago: evento.dataValues.metodopago,
+   adeudado: evento.dataValues.adeudado,
+ }));
+
+ const pagosTransformados = pagos.map((evento) => ({
+   pagos: evento.dataValues.dia,
+ }));
+   const resultadoTransformado = organizarDatos(contratosTransformados, pagosTransformados);
+    return res.status(200).json(resultadoTransformado);
+ }
+    
+
+   
+
+   
+  } catch (error) {
+    console.error('Error en getContratosFiltrados:', error);
+    return res.status(500).send('Error interno del servidor');
+  }
+}
+
+
+const organizarDatos = (datos:any, datosPagos:any) => {
+  const resultado:any = {};
+
+  datos.forEach((elemento:any) => {
+    const fecha = new Date(elemento.dia);
+    const año = fecha.getFullYear();
+    const mes = fecha.getMonth() + 1; // Sumamos 1 ya que los meses en JavaScript van de 0 a 11
+
+    // Inicializamos las propiedades si no existen
+    resultado[año] = resultado[año] || {};
+    resultado[año].mes = resultado[año].mes || {};
+    resultado[año].mes[mes] = resultado[año].mes[mes] || {
+      cargados: 0,
+      pagados: 0,
+      cancelados: 0
+    };
+
+    // Incrementamos los contadores según el método de pago y el adeudo
+    resultado[año].mes[mes].cargados += 1;
+
+    if (elemento.adeudado === '0') {
+      resultado[año].mes[mes].cancelados += 1;
+    }
+    // Puedes agregar la lógica para contabilizar los pagados aquí
+  });
+
+  // Agregar la lógica para contabilizar los pagos
+  datosPagos.forEach((pago:any) => {
+    const fechaPago = new Date(pago.pagos);
+    const añoPago = fechaPago.getFullYear();
+    const mesPago = fechaPago.getMonth() + 1;
+
+    // Verificar si la propiedad ya está inicializada
+    resultado[añoPago] = resultado[añoPago] || {};
+    resultado[añoPago].mes = resultado[añoPago].mes || {};
+    resultado[añoPago].mes[mesPago] = resultado[añoPago].mes[mesPago] || {
+      cargados: 0,
+      pagados: 0,
+      cancelados: 0
+    };
+
+    // Incrementar el contador de pagados
+    resultado[añoPago].mes[mesPago].pagados += 1;
+  });
+
+  // Rellenar los meses que faltan con valores predeterminados
+  for (const año in resultado) {
+    for (let mes = 1; mes <= 12; mes++) {
+      resultado[año].mes[mes] = resultado[año].mes[mes] || {
+        cargados: 0,
+        pagados: 0,
+        cancelados: 0
+      };
+    }
+  }
+
+  return resultado;
+};
--- src/component/estadisticas/router.ts
+++ src/component/estadisticas/router.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { verifyToken } from "../../middlewares/verifysingup";
-import {getContratos,getCargas,getEventos,getContratosVendors} from './controller'
+import {getContratos,getCargas,getEventos,getContratosVendors,getContratosFiltrados} from './controller'
 const router = Router()
 
   router.post('/sta/cargas',[verifyToken],getCargas)
   router.post('/sta/contratos',[verifyToken],getContratos)
   router.post('/sta/eventos',[verifyToken],getEventos)
   router.post('/sta/venCargas',[verifyToken],getContratosVendors)
+  router.post('/sta/contratosest',[verifyToken],getContratosFiltrados)
 
 export default router
